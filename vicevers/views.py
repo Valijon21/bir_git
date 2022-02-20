7349f6149748fe22627d0c22ccbc02080a46a691
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-21 03:23:29 +0500

    dastur tuzib bulindi text teskari yozdi

--- vicevers/views.py
+++ vicevers/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+
 def home(request):
     return render(request,'home.html')
+
+
 def user(request):
-    return render(request,'user.html')
+    user_text = request.GET['usertext']
+    reverse_text = user_text[::-1]
+    return render(request,'user.html',{'usertext':user_text,
+                                       'reverse':reverse_text})
