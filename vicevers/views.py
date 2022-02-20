8555e692eafb5860cfbffa42181b9be139838227
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-02-21 03:50:56 +0500

    matinda suzni sonini topishni qushdik

--- vicevers/views.py
+++ vicevers/views.py
@@ -7,5 +7,8 @@ def home(request):
 def user(request):
     user_text = request.GET['usertext']
     reverse_text = user_text[::-1]
+    count_words = len((user_text).split())
+    print(count_words)
     return render(request,'user.html',{'usertext':user_text,
-                                       'reverse':reverse_text})
+                                       'reverse':reverse_text,
+                                       'countwords': count_words})
